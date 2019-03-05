46d530117a3c860f846271f39a9e8c5ec73b6604
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2019-03-05 21:21:11 +0330

    Fix 404 page problem temporarily

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,12 +3,33 @@ import React from "react"
 import {Container} from "../components/Container"
 import { SEO } from "../components/SEO"
 
-const NotFoundPage = () => (
-  <Container>
-    <SEO title="404: Not found" />
+const NotFoundPage = ({ data }) => (
+  <Container logo={data.logo} seo={data.seo}>
+    <SEO title="404: Not found" seo={data.seo}/>
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Container>
 )
 
 export default NotFoundPage;
+
+export const query = graphql`
+query {
+  logo: file(relativePath: { eq: "icon-face.png" }) {
+    childImageSharp {
+      fixed(height: 40, width: 40) {
+        ...GatsbyImageSharpFixed
+      }
+    }
+  }
+  seo: site {
+    siteMetadata {
+      name
+      title
+      description
+      author
+      keywords
+    }
+  }
+}
+`;
