21f5310df5bc59f8808b58228267a2151539fcf2
Soroush Torkzadeh <soroush.torkzadeh@gmail.com>
2019-09-15 19:17:01 +0200

    Set service tiles yellow

--- src/components/ServiceTile/index.js
+++ src/components/ServiceTile/index.js
@@ -8,7 +8,7 @@ export const ServiceTile = ({ services }) => {
         services.edges.map((item) => (
           <div className="tile is-parent" key={item.node.id}>
             <a href={`/service/${item.node.slug}`} title={item.node.subtitle}>
-              <article className="tile is-child box">
+              <article className="tile is-child notification is-warning">
                 <p className="title is-size-4">{item.node.title}</p>
                 <p className="subtitle is-size-6">{item.node.subtitle}</p>
                 <Img
